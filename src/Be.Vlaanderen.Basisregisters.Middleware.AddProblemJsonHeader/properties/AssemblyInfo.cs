ed8307649e71fa1936972428e5ab0e4c1a98691c
emalfroy <egon.malfroy@gmail.com>
2024-03-07 16:57:21 +0100

    ci: build + assemblyinfo

---
+++ src/Be.Vlaanderen.Basisregisters.Middleware.AddProblemJsonHeader/properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Vlaamse basisregisters")]
+[assembly: AssemblyDescription("ASP.NET Core MVC Middleware to add 'application/problem+json to a request's Accept header'.")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("ca285750-2486-4d17-9184-4b3b042670b1")]
+
+[assembly: NeutralResourcesLanguage("en")]
